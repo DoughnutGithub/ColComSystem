3b1e14d13a82a92bba464d86cad4cbc4d99817f9
DoughnutGithub <1627287292@qq.com>
2014-07-30 21:50:30 +0800

    添加基本页面

---
+++ ColComSystem/Controllers/DocumentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColComSystem.Controllers
+{
+    public class DocumentController : Controller
+    {
+        //
+        // GET: /Document/
+
+        public ActionResult Documents()
+        {
+            return View();
+        }
+
+        public ActionResult DocumentDetails()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ColComSystem/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColComSystem.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+
+
+    }
+}
